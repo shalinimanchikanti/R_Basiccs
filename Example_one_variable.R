f55f7f09a8eb476d003abdd9c966b23b710eee7b
shalinimanchikanti <mshalini04@gmail.com>
2018-03-29 15:02:13 -0700

    Added Price_Histogram png

--- Example_one_variable.R
+++ Example_one_variable.R
@@ -22,3 +22,27 @@ diamonds$color
 #to see how many are ordered factors
 summary(diamonds)
 
+# Create a histogram of the price of
+# all the diamonds in the diamond data set.
+
+library(ggplot2)
+hist(diamonds$price)
+price_hist <- ggplot(diamonds, aes(x = price)) +
+        geom_histogram(color = ('black'), fill = ("white")) 
+
+summary(diamonds$price)
+
+#count the number of diamonds with price given a condition
+diamonds$price < 500
+sum(diamonds$price < 500)
+length(which(diamonds$price < 500))
+
+
+diamonds$price >= 15000
+sum(diamonds$price >= 15000)
+length(which(diamonds$price >= 15000))
+
+diamonds$price < 250
+sum(diamonds$price < 250)
+length(which(diamonds$price < 250))
+
